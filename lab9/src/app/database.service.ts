4d4e1f332247f086a94646e4ac8e0b0f9ac5d80b
DumIlin <111404925+DumIlin@users.noreply.github.com>
2022-11-19 17:27:08 +1100

    Angular

---
+++ lab9/src/app/database.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+
+const httpOptions = {
+  headers: new HttpHeaders({"Content-Type": "application/json"})
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DatabaseService {
+  constructor(private http: HttpClient) {}
+  
+  getParcels(){
+    return this.http.get("/parcel")
+  }
+
+  addParcel(data:any){
+    return this.http.put( "/sender/parcel", data, httpOptions)
+  }
+  getSenders(){
+    return this.http.get("/sender")
+  }
+
+  addSender(data:any){
+    return this.http.post( "/sender", data, httpOptions)
+  }
+
+  delSender(id:any){
+    //console.log(id)
+    let url = "/sender/" + id
+    return this.http.delete(url, httpOptions);
+  }
+
+}
